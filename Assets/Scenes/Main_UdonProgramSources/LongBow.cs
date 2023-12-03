8d160dda754759febb63b447a47bec7c1ac616e1
ARigitano <antoine@rigitano.com>
2023-12-03 19:01:37 +0100

    Integrated first assets
    WIP: bow shooting

---
+++ Assets/Scenes/Main_UdonProgramSources/LongBow.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LongBow : UdonSharpBehaviour
+{
+    public GameObject _arrowPrefab;
+
+    public override void OnPickupUseDown()
+    {
+        base.OnPickupUseDown();
+
+        GameObject arrow = Instantiate(_arrowPrefab, this.transform.position, this.transform.rotation);
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward);
+    }
+}
