ae1acf0afb6004af7369050f1c57b38be726e2a9
ARigitano <antoine@rigitano.com>
2024-03-19 18:42:35 +0100

    Knight moves towards door

---
+++ Assets/Scenes/Main_UdonProgramSources/Attacker.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.AI;
+
+public class Attacker : UdonSharpBehaviour
+{
+    public Transform door; //Position of the castle door.
+    private NavMeshAgent agent; //NavMeshAgent of the attacker.
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Move");
+    }
+
+    //The attacker moves toward the castle door.
+    public void Move()
+    {
+        agent.destination = door.position;
+    }
+}
